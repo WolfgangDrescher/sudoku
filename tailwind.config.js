c1ac7562ed8099f98bd79823b6c5e0e2d3fc0a5d
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-12-31 12:30:02 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    corePlugins: {
+        aspectRatio: false,
+    },
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+    ],
+};

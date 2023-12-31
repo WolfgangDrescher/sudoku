2baedb41f3fe6370386bc28be333fd791ca8c0c3
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2023-12-31 23:20:40 +0100

    Display resolved values blue

--- components/SudokuCell.vue
+++ components/SudokuCell.vue
@@ -24,6 +24,7 @@ const displayHighlightNumber = computed(() => {
         }">
             <div class="text-6xl" :class="{
                 'font-bold': cell.value === cell.given,
+                'text-blue-500': cell.value === cell.resolved,
             }">
                 {{ cell.value }}
             </div>
